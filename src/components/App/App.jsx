0ab8732c5454fb820bd7e9af49814e210f5808dd
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2021-12-25 16:08:51 +0200

    createSlice

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,26 @@
-import { useState, useEffect } from "react";
-// import React, { Component } from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { nanoid } from "nanoid";
 import ContactForm from "../ContactForm/ContactForm.jsx";
 import Filter from "../SearchFilter/SearchFilter.jsx";
 import ContactList from "../ContactList/ContactList.jsx";
 import * as storage from "../../services/localStorage";
+import * as actions from "../../redux/contactSlice/contactsSlice";
 
 const App = () => {
-  const [filter, setFilter] = useState("");
-  const [contacts, setContacts] = useState([]);
+  // const [filter, setFilter] = useState("");
+  // const [contacts, setContacts] = useState([]);
+
+  const contacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.contacts.filter);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const contacts = storage.get("contacts");
     if (contacts && contacts.length > 0) {
-      setContacts(contacts);
+      dispatch(actions.setContacts(contacts));
     }
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     storage.save("contacts", contacts);
@@ -35,12 +40,15 @@ const App = () => {
     if (name === "") {
       alert(`Please type your info in the field. It is empty.`);
     } else {
-      setContacts([...contacts, contactName]);
+      dispatch(actions.addContacts(contactName));
     }
   };
 
+  // const deleteContact = (id) => {
+  //   dispatch(contacts.filter((contact) => contact.id !== id));
+  // };
   const deleteContact = (id) => {
-    setContacts(contacts.filter((contact) => contact.id !== id));
+    dispatch(actions.deleteContacts(id));
   };
 
   const filterContacts = () => {
@@ -50,7 +58,7 @@ const App = () => {
   };
 
   const updateFilter = (event) => {
-    setFilter(event.currentTarget.value);
+    dispatch(actions.updateFilter(event.currentTarget.value));
   };
 
   return (
@@ -65,84 +73,5 @@ const App = () => {
     </>
   );
 };
-
-// class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: "",
-//   };
-
-// componentDidMount() {
-//   const savedContacts = storage.get("contacts");
-//   if (savedContacts) {
-//     this.setState({ contacts: savedContacts });
-//   }
-// }
-
-// componentDidUpdate(prevProps, prevState) {
-//   const { contacts } = this.state;
-//   if (prevState.contacts !== contacts) {
-//     storage.save("contacts", contacts);
-//   }
-// }
-
-// addNewContact = (name, number) => {
-//   const contactName = { name, number, id: nanoid() };
-//   const normalizedName = name.toLowerCase();
-//   const duplicateName = this.state.contacts.find(
-//     (contact) => contact.name.toLowerCase() === normalizedName
-//   );
-
-//   if (duplicateName) {
-//     alert(`${name} is already in contacts.`);
-//     return;
-//   }
-//   if (name === "") {
-//     alert(`Please type your info in the field. It is empty.`);
-//   } else {
-//     this.setState(({ contacts }) => ({
-//       contacts: [...contacts, contactName],
-//     }));
-//   }
-// };
-
-// deleteContact = (id) => {
-//   this.setState((prevState) => ({
-//     contacts: prevState.contacts.filter((contact) => contact.id !== id),
-//   }));
-// };
-
-// filterContacts = () => {
-//   const { filter, contacts } = this.state;
-
-//   return contacts.filter(({ name }) =>
-//     name.toLowerCase().includes(filter.toLowerCase())
-//   );
-// };
-
-// updateFilter = (event) => {
-//   const { value } = event.target;
-//   this.setState({ filter: value });
-// };
-
-//   render() {
-//     const { filter } = this.state;
-//     const filteredContacts = this.filterContacts();
-//     return (
-//       <>
-//         <div>
-//           <h1>Phonebook</h1>
-//           <ContactForm onSubmit={this.addNewContact} />
-//           <h2>Contacts</h2>
-//           <Filter filter={filter} onChange={this.updateFilter} />
-//           <ContactList
-//             contacts={filteredContacts}
-//             onClick={this.deleteContact}
-//           />
-//         </div>
-//       </>
-//     );
-//   }
-// }
 
 export default App;
